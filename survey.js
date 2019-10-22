7ec840a94304c9f990af08c7039dada9a5966b18
Teddy Leung <teddy.ph.leung@gmail.com>
2019-10-22 15:56:13 -0700

    Removed useless answer properties.

--- survey.js
+++ survey.js
@@ -8,31 +8,24 @@ const rl = readline.createInterface({
 const questions = {
   0: {
     question: "What's your name? Nicknames are also acceptable :)",
-    answer: null
   },
   1: {
     question: "What's an activity you like doing?",
-    answer: null
   },
   2: {
     question: "What do you listen to while doing that?",
-    answer: null
   },
   3: {
     question: "Which meal is your favourite (eg: dinner, brunch, etc.)",
-    answer: null
   },
   4: {
     question: "What's your favourite thing to eat for that meal?",
-    answer: null
   },
   5: {
     question: "Which sport is your absolute favourite?",
-    answer: null
   },
   6: {
     question: "What is your superpower? In a few words, tell us what you are amazing at!",
-    answer: null
   }
 };
 
